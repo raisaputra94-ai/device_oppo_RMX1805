560725cac37a1382e05915650c3e4536bd49ba27
raisaputra94-ai <raisaputra94@gmail.com>
2026-07-21 13:32:21 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
-git clone https://github.com/LinuxGuy312/vendor_oppo_RMX1805.git -b 11 vendor/oppo/RMX1805
+# git clone https://github.com/selfmusing/vendor_bcr.git vendor/bcr
+# git clone https://github.com/LinuxGuy312/vendor_oppo_RMX1805.git -b 11 vendor/oppo/RMX1805
 
-git clone https://github.com/LinuxGuy312/android_kernel_realme_RMX1805.git kernel/oppo/RMX1805
-cd kernel/oppo/RMX1805
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
-cd ../../../
+# git clone https://github.com/LinuxGuy312/android_kernel_realme_RMX1805.git kernel/oppo/RMX1805
+# cd kernel/oppo/RMX1805
+# curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
+# cd ../../../
